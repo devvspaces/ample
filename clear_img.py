71524485107ad5f0f5f2debb305d4a9fcbf276ed
n3trob3 <netrobeweb@gmail.com>
2021-03-08 22:17:49 +0000

    'chromeOptionsAdded'

--- clear_img.py
+++ clear_img.py
@@ -4,12 +4,24 @@ from selenium.webdriver.common.keys import Keys
 import CONFIG
 
 
+option = webdriver.ChromeOptions()
+#option.binary_location = GOOGLE_CHROME_BIN
+option.add_argument("--headless")  
+option.add_argument("--disable-dev-shm-usage")
+option.add_argument("--no-sandbox")
+option.add_argument('--disable-gpu')
+option.add_argument("--incognito")
+
+
+# browser = webdriver.Chrome(executable_path=r"D:\software\chromedriver", chrome_options=option)
+
+
 def get_event_image(url):
     # try:
     cl='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 a8c37x1j p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l'
 
     # Login facebook with selenium webdriver
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chrome_options=option)
     driver.get('http://www.facebook.com')
 
     username = driver.find_element_by_id('email')
