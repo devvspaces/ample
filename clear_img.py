901d738f1c7503ff769ebb0332456d4cdee12748
n3trob3 <netrobeweb@gmail.com>
2021-03-08 23:47:48 +0000

    'update'

--- clear_img.py
+++ clear_img.py
@@ -7,7 +7,7 @@ import CONFIG
 option = webdriver.ChromeOptions()
 #option.binary_location = GOOGLE_CHROME_BIN
 # option.add_argument("--headless")  
-# option.add_argument("--disable-dev-shm-usage")
+option.add_argument("--disable-dev-shm-usage")
 option.add_argument("--no-sandbox")
 option.add_argument("--disable-setuid-sandbox")
 # option.add_argument('--disable-gpu')
