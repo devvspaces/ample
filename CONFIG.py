16640581694facfe942bd53a3edce8117dab3d09
n3trob3 <netrobeweb@gmail.com>
2021-03-15 14:01:46 +0000

    'update'

--- CONFIG.py
+++ CONFIG.py
@@ -1,8 +1,5 @@
-# FB_EMAIL = 'sadatakash018@gmail.com'
-# FB_PASSWORD = 'acimis15900#6'
-
-FB_EMAIL = '09033295156'
-FB_PASSWORD = 'alakio12345@#2*6*7*0*8'
+FB_EMAIL = 'sadatakash018@gmail.com'
+FB_PASSWORD = 'acimis15900#6'
 
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
