5d8d4156f2849896ab24ca88748b50b094e420dd
n3trob3 <netrobeweb@gmail.com>
2021-03-22 13:01:32 +0000

    'update'

--- app.py
+++ app.py
@@ -178,7 +178,10 @@ def hd_image_handler():
         logger.debug('Got to GET the hd images function')
         try:
             for eid in redos:
-                photo = gei(eid)
+                # photo = gei(eid)
+                event = query_db("SELECT photo from events WHERE id ='"+str(eid)+"'"):
+                print(event)
+                # Select the event from the db
                 if photo is not None and (eid.find('t1.0-0/cp0/e15/') != -1):
                     sql = f"UPDATE `events` SET photo={photo} WHERE id ='"+str(eid)+"'"
                     insert_val = upsert_db(sql)
